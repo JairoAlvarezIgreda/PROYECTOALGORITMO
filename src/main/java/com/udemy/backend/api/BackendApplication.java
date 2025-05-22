fff119cca12105baa38e5e0a06a6165028b4c416
zincognity <67128655+zincognity@users.noreply.github.com>
2025-05-22 11:33:47 -0500

    build(gradle): update dependencies and ignore environment files
    - Add `spring-boot-starter` and `mysql-connector-j` dependencies to support application functionality and database connectivity.
    - Update `.gitignore` to exclude `.env` files for environment variable security.
    - Rename the Gradle root project to `backend.api` for better project identification.
    - Remove unused application files, including main application class, properties, and tests, likely for restructuring or cleanup.

--- src/main/java/com/udemy/backend/api/BackendApplication.java
+++ src/main/java/com/udemy/backend/api/BackendApplication.java
@@ -1,9 +1,12 @@
-package com.udemy.backend;
+package com.udemy.backend.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/**
+ * * Clase principal que contendrá la función main para inicializar el proyecto.
+ */
 public class BackendApplication {
 
   public static void main(String[] args) {
