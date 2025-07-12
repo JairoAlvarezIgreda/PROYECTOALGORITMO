2079842e8bb2b13b0d2f763f06aee3e06242488d
zincognity <67128655+zincognity@users.noreply.github.com>
2025-07-12 17:53:54 -0500

    feat(module): Implements ModuleRepository interface
    This commit makes the ImplModuleRepository class implement the ModuleRepository interface.
    This ensures that the repository adheres to the contract defined by the interface, improving code maintainability and testability.

--- src/main/java/com/udemy/backend/api/course/module/core/adapter/out/repository/impl/ImplModuleRepository.java
+++ src/main/java/com/udemy/backend/api/course/module/core/adapter/out/repository/impl/ImplModuleRepository.java
@@ -3,13 +3,14 @@ package com.udemy.backend.api.course.module.core.adapter.out.repository.impl;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
+import com.udemy.backend.api.course.module.core.adapter.out.repository.ModuleRepository;
 import com.udemy.backend.api.course.module.core.domain.model.Module;
 import com.udemy.backend.api.shared.application.repository.GlobalBasicRepository;
 
 @Primary
 @Repository
 public class ImplModuleRepository
-    extends GlobalBasicRepository<Module, Long> {
+    extends GlobalBasicRepository<Module, Long> implements ModuleRepository {
   public ImplModuleRepository() {
     super(Module::getId);
   }
